ad65226a72aa91e5d74d8bcac32c6f0d51156403
Rasel <rasel.ahammed@wcgclinical.com>
2024-09-09 00:21:00 +0600

    Same day conflict check added

--- Frontend/table.js
+++ Frontend/table.js
@@ -1,6 +1,9 @@
 // Get the number of rows and columns
-const rows = 10;
+const rows = 5;
 const columns = 5;
+const totalSemesters = 2;
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
 
 // Get the container where the table will be displayed
 const tableContainer = document.getElementById('tableContainer');
@@ -15,22 +18,38 @@ table.style.borderCollapse = 'collapse'; // Make sure borders are collapsed
 var cellIdCounter = 0;
 // Generate rows and columns
 for (let i = 0; i < rows; i++) {
-    const tr = document.createElement('tr');
 
-    for (let j = 0; j < columns; j++) {
-        const td = document.createElement('td');
-        // td.textContent = `cell-${cellIdCounter}`; // Example content
-        td.style.border = '1px solid black'; // Set border for table cells
-        td.style.padding = '8px'; // Add some padding
-        td.style.height = '40px';
-        td.style.width = '120px';
-        td.id = `cell-${cellIdCounter}`;
-        tr.appendChild(td);
+    for(let j = 0; j < totalSemesters; j++)
+    {
+        const tr = document.createElement('tr');
 
-        cellIdCounter++
+        if (j == 0) {
+            const td = document.createElement('td');
+            td.setAttribute("rowspan", 2);
+            td.style.border = '1px solid black'; // Set border for table cells
+            td.style.padding = '8px'; // Add some padding
+            td.style.height = '40px';
+            td.style.width = '120px';
+            td.style.fontWeight = 'bold';
+            td.innerHTML = days[i];
+            tr.appendChild(td);
+        }
+
+        for (let k = 0; k < columns; k++) {
+            const td = document.createElement('td');
+            // td.textContent = `cell-${cellIdCounter}`; // Example content
+            td.style.border = '1px solid black'; // Set border for table cells
+            td.style.padding = '8px'; // Add some padding
+            td.style.height = '40px';
+            td.style.width = '120px';
+            td.id = `cell-${cellIdCounter}`;
+            tr.appendChild(td);
+
+            cellIdCounter++
+        }
+
+        table.appendChild(tr);
     }
-
-    table.appendChild(tr);
 }
 
 // Append the table to the container
@@ -51,6 +70,7 @@ async function populateData() {
         console.error('Error fetching data from API:', error);
         return;
     }
+
     data.forEach(item => {
         var element = document.getElementById(`cell-${item.cellNumber}`);
         if (element)
